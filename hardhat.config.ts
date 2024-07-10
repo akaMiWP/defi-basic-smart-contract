1127fb153486bcc5bcd383291e3f9d1cf3ed2d8d
akaMiWP <bangkokappdevelopment@gmail.com>
2024-07-10 19:29:36 +0700

    Added dotenv

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,15 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+require("dotenv").config;
+
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_ALCHEMY_URL,
+    },
+  },
 };
 
 export default config;
