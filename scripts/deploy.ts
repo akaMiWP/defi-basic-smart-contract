51320ff59bd04c7acf23fa981084a512968505ec
akaMiWP <bangkokappdevelopment@gmail.com>
2024-07-13 17:35:18 +0700

    Edited deployment script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -15,7 +15,34 @@ async function main() {
     JSON.bytecode,
     wallet
   );
-  const contract = await ContractFactory.deploy();
+
+  const priceFeeds = [
+    {
+      ticker: "BTC/ETH",
+      aggregatorAddress: "0x5fb1616F78dA7aFC9FF79e0371741a747D2a7F22",
+    },
+    {
+      ticker: "BTC/USD",
+      aggregatorAddress: "0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43",
+    },
+    {
+      ticker: "DAI/USD",
+      aggregatorAddress: "0x14866185B1962B63C3Ea9E03Bc1da838bab34C19",
+    },
+    {
+      ticker: "ETH/USD",
+      aggregatorAddress: "0x694AA1769357215DE4FAC081bf1f309aDC325306",
+    },
+    {
+      ticker: "LINK/ETH",
+      aggregatorAddress: "0x42585eD362B3f1BCa95c640FdFf35Ef899212734",
+    },
+    {
+      ticker: "LINK/USD",
+      aggregatorAddress: "0xc59E3633BAAC79493d908e63626716e204A45EdF",
+    },
+  ];
+  const contract = await ContractFactory.deploy(priceFeeds);
   console.log("The contract has been deployed", await contract.getAddress());
 }
 
