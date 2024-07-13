9b4c6be46c14155ae20118ae2dd12dddfdaa021a
akaMiWP <bangkokappdevelopment@gmail.com>
2024-07-13 13:32:35 +0700

    Added depositToken and swapTokens  to cover basic usage as a dex

--- contracts/DefiConsumerV3.sol
+++ contracts/DefiConsumerV3.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.24;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/interfaces/IERC20.sol";
+import "@openzeppelin/contracts/interfaces/IERC20Metadata.sol";
 
 contract DefiConsumerV3 is Ownable {
     mapping(string => AggregatorV3Interface) public priceFeeds;
@@ -31,8 +32,14 @@ contract DefiConsumerV3 is Ownable {
     }
 
     function getChainlinkDataFeedLatestAnswer(
-        string calldata priceFeed
+        address token1,
+        address token2
     ) public view returns (int) {
+        string memory token1Symbol = IERC20Metadata(token1).symbol();
+        string memory token2Symbol = IERC20Metadata(token2).symbol();
+        string memory priceFeedTicker = string(
+            abi.encodePacked(token1Symbol, "/", token2Symbol)
+        );
         // prettier-ignore
         (
             /* uint80 roundID */,
@@ -40,11 +47,26 @@ contract DefiConsumerV3 is Ownable {
             /*uint startedAt*/,
             /*uint timeStamp*/,
             /*uint80 answeredInRound*/
-        ) = priceFeeds[priceFeed].latestRoundData();
+        ) = priceFeeds[priceFeedTicker].latestRoundData();
         return answer;
     }
 
     function depositToken(address token, uint256 amount) external onlyOwner {
-        require(IERC20(token).transfer(msg.sender, amount), "Deposit failed");
+        require(
+            IERC20(token).transferFrom(msg.sender, address(this), amount),
+            "Deposit failed"
+        );
+    }
+
+    function swapTokens(
+        address token1,
+        address token2,
+        uint256 amount
+    ) external {
+        int256 price = getChainlinkDataFeedLatestAnswer(token1, token2);
+        require(price > 0, "Invalid price");
+        uint256 amountOfToken2 = (amount * uint256(price)) / 1e18;
+        require(IERC20(token1).transferFrom(msg.sender, address(this), amount));
+        require(IERC20(token2).transfer(msg.sender, amountOfToken2));
     }
 }
