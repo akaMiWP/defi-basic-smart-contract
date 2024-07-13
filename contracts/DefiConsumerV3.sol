b975db775fbb4d64ec05374ce30b93fdbe04bd7a
akaMiWP <bangkokappdevelopment@gmail.com>
2024-07-13 15:18:17 +0700

    Removed an unused file + Working on a test file

--- contracts/DefiConsumerV3.sol
+++ contracts/DefiConsumerV3.sol
@@ -8,27 +8,37 @@ import "@openzeppelin/contracts/interfaces/IERC20.sol";
 import "@openzeppelin/contracts/interfaces/IERC20Metadata.sol";
 
 contract DefiConsumerV3 is Ownable {
+    struct PriceFeedPair {
+        string ticker;
+        address aggregatorAddress;
+    }
+
     mapping(string => AggregatorV3Interface) public priceFeeds;
 
-    constructor() {
-        priceFeeds["BTC/ETH"] = AggregatorV3Interface(
-            0x5fb1616F78dA7aFC9FF79e0371741a747D2a7F22
-        );
-        priceFeeds["BTC/USD"] = AggregatorV3Interface(
-            0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43
-        );
-        priceFeeds["DAI/USD"] = AggregatorV3Interface(
-            0x14866185B1962B63C3Ea9E03Bc1da838bab34C19
-        );
-        priceFeeds["ETH/USD"] = AggregatorV3Interface(
-            0x694AA1769357215DE4FAC081bf1f309aDC325306
-        );
-        priceFeeds["LINK/ETH"] = AggregatorV3Interface(
-            0x42585eD362B3f1BCa95c640FdFf35Ef899212734
-        );
-        priceFeeds["LINK/USD"] = AggregatorV3Interface(
-            0xc59E3633BAAC79493d908e63626716e204A45EdF
-        );
+    constructor(PriceFeedPair[] memory priceFeedPairs) {
+        // priceFeeds["BTC/ETH"] = AggregatorV3Interface(
+        //     0x5fb1616F78dA7aFC9FF79e0371741a747D2a7F22
+        // );
+        // priceFeeds["BTC/USD"] = AggregatorV3Interface(
+        //     0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43
+        // );
+        // priceFeeds["DAI/USD"] = AggregatorV3Interface(
+        //     0x14866185B1962B63C3Ea9E03Bc1da838bab34C19
+        // );
+        // priceFeeds["ETH/USD"] = AggregatorV3Interface(
+        //     0x694AA1769357215DE4FAC081bf1f309aDC325306
+        // );
+        // priceFeeds["LINK/ETH"] = AggregatorV3Interface(
+        //     0x42585eD362B3f1BCa95c640FdFf35Ef899212734
+        // );
+        // priceFeeds["LINK/USD"] = AggregatorV3Interface(
+        //     0xc59E3633BAAC79493d908e63626716e204A45EdF
+        // );
+        for (uint i = 0; i < priceFeedPairs.length; i++) {
+            priceFeeds[priceFeedPairs[i].ticker] = AggregatorV3Interface(
+                priceFeedPairs[i].aggregatorAddress
+            );
+        }
     }
 
     function getChainlinkDataFeedLatestAnswer(
--- test/DefiConsumerV3.ts
+++ test/DefiConsumerV3.ts
@@ -2,9 +2,13 @@ import { ethers } from "hardhat";
 
 import ERC20MockJSON from "../artifacts/contracts/Mocks/ERC20Mock.sol/ERC20Mock.json";
 import AggregatorV3MockJSON from "../artifacts/contracts/Mocks/AggregatorV3Mock.sol/AggregatorV3Mock.json";
+import DefiConsumerV3JSON from "../artifacts/contracts/DefiConsumerV3.sol/DefiConsumerV3.json";
 
 describe("DefiConsumerV3", () => {
   let owner, addr1, addr2;
+  let token1, token2;
+  let aggregatorV3;
+
   beforeEach(async () => {
     [owner, addr1, addr2] = await ethers.getSigners();
 
@@ -14,6 +18,46 @@ describe("DefiConsumerV3", () => {
       ERC20MockJSON.bytecode,
       owner
     );
-    ERC20Mock.deploy();
+
+    token1 = await ERC20Mock.deploy("Token1", "TK1", 100);
+    token2 = await ERC20Mock.deploy("Token2", "TK2", 200);
+
+    // Deploy mock AggregatorV3 pricefeed;
+    const AggregatorV3Mock = new ethers.ContractFactory(
+      AggregatorV3MockJSON.abi,
+      AggregatorV3MockJSON.bytecode,
+      owner
+    );
+    aggregatorV3 = await AggregatorV3Mock.deploy(0.01 * (10 ^ 18));
+
+    // Deploy Defi contract
+    const priceFeedPairs = [
+      { ticker: "TK1/TK2", aggregatorAddress: await aggregatorV3.getAddress() },
+    ];
+    const DefiConsumerV3 = new ethers.ContractFactory(
+      DefiConsumerV3JSON.abi,
+      DefiConsumerV3JSON.bytecode,
+      owner
+    );
+
+    const defiConsumerV3 = await DefiConsumerV3.deploy(priceFeedPairs);
+
+    // Log addresses
+    console.log("Deployed Token 1", await token1.getAddress());
+    console.log("Deployed Token 2", await token2.getAddress());
+    console.log("Deployed Aggregator", await aggregatorV3.getAddress());
+    console.log("Deployed DefiConsumverV3", await defiConsumerV3.getAddress());
+
+    // Fund DeFi contract with token2
+    token2.connect(owner).approve(await defiConsumerV3.getAddress(), 50);
+    defiConsumerV3
+      .connect(owner)
+      .transferFrom(owner, await defiConsumerV3.getAddress(), 50);
+  });
+
+  describe("Test swap functionality", () => {
+    it("Hello World", () => {
+      //   console.log("Hello World");
+    });
   });
 });
